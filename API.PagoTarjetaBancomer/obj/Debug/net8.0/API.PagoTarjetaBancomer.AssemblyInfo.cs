fa8addf9aaf111c64cd44a090b1de0f8f3c0f034
Javier Jiménez <javier.jimenez@efactornetwork.com>
2025-11-24 13:22:50 -0600

    ok

--- API.PagoTarjetaBancomer/obj/Debug/net8.0/API.PagoTarjetaBancomer.AssemblyInfo.cs
+++ API.PagoTarjetaBancomer/obj/Debug/net8.0/API.PagoTarjetaBancomer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API.PagoTarjetaBancomer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ee0baa40404640f606cc09eac820aabb9da8901")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b54e9dfb23ef9a0d32524007d87691ab278f8e37")]
 [assembly: System.Reflection.AssemblyProductAttribute("API.PagoTarjetaBancomer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API.PagoTarjetaBancomer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
